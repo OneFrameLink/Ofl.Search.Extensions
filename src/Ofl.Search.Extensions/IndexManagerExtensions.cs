9bd058d91e3935a0fb3981a59b38faf10c06942d
casperOne <casperOne@caspershouse.com>
2019-01-09 16:18:40 -0500

    Initial commit.

---
+++ src/Ofl.Search.Extensions/IndexManagerExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ofl.Search
+{
+    public static class IndexManagerExtensions
+    {
+        public static async Task UpsertAsync<T>(this IIndexManager indexManager, string indexName, IEnumerable<T> source,
+            CancellationToken cancellationToken)
+            where T : class
+        {
+            // Validate parameters.
+            if (indexManager == null) throw new ArgumentNullException(nameof(indexManager));
+            if (string.IsNullOrWhiteSpace(indexName)) throw new ArgumentNullException(nameof(indexName));
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            // Get the index, cast to the type of T.
+            var index = (IIndex<T>) await indexManager.GetIndexAsync(indexName, cancellationToken).ConfigureAwait(false);
+
+            // Get the write operations.
+            IIndexWriteOperations<T> wo = await index.GetWriteOperationsAsync(cancellationToken).ConfigureAwait(false);
+
+            // Update.
+            await wo.UpsertAsync(source, cancellationToken).ConfigureAwait(false);
+        }
+        public static async Task<SearchResponse<T>> SearchAsync<T>(this IIndexManager indexManager, string indexName, SearchRequest request,
+            CancellationToken cancellationToken)
+            where T : class
+        {
+            // Validate parameters.
+            if (indexManager == null) throw new ArgumentNullException(nameof(indexManager));
+            if (string.IsNullOrWhiteSpace(indexName)) throw new ArgumentNullException(nameof(indexName));
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            // Get the index.
+            var index = (IIndex<T>) await indexManager.GetIndexAsync(indexName, cancellationToken).ConfigureAwait(false);
+
+            // Get the read operations.
+            IIndexReadOperations<T> ro = await index.GetReadOperationsAsync(cancellationToken).ConfigureAwait(false);
+
+            // Search.
+            return await ro.SearchAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Ofl.Search.Extensions/SearchRequestExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ofl.Search
+{
+    public static class SearchRequestExtensions
+    {
+        public static void Validate(this SearchRequest request)
+        {
+            // Call the overload.
+            request.Validate(nameof(request));
+        }
+
+        public static void Validate(this SearchRequest request, string parameter)
+        {
+            // Validate parameters.
+            // NOTE: Order is important here, as first parameter can't be validated with a proper message
+            // without second parameter being set properly.
+            if (string.IsNullOrWhiteSpace(parameter)) throw new ArgumentNullException(nameof(parameter));
+            if (request == null) throw new ArgumentNullException(parameter);
+
+            // Don't validate query, nothing means return nothing.
+            if (request.Skip < 0) throw new ArgumentException($"The { nameof(request.Skip) } property on the { parameter } parameter must be a non-negative number.");
+            if (request.Take < 0) throw new ArgumentException($"The { nameof(request.Take) } property on the { parameter } parameter must be a non-negative number.");
+
+            // Minimum score.
+            if (request.MinimumScore < 0) throw new ArgumentException($"The { nameof(request.MinimumScore)} property on the { parameter } parameter must be a non-negative number.");
+        }
+    }
+}
