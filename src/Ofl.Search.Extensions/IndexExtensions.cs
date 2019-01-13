4b4f88a7c92a35191117ec2d66db7107b070f76b
casperOne <casperOne@caspershouse.com>
2019-01-13 17:34:45 -0500

    Updated references, added RecreateIndexAsync as an extension method.

---
+++ src/Ofl.Search.Extensions/IndexExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ofl.Search
+{
+    public static class IndexExtensions
+    {
+        public static async Task RecreateIndexAsync(this IIndex index, CancellationToken cancellationToken)
+        {
+            // Validate parameters.
+            if (index == null) throw new ArgumentNullException(nameof(index));
+
+            // Destroy the index.
+            await index.DestroyAsync(cancellationToken).ConfigureAwait(false);
+
+            // Create the index.
+            await index.CreateAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
